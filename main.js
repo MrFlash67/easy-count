80452ec3f9fc1aed5d73f2f4788c4bfbe0ff2252
MrFlash67 <mrflash67@noreply.github.com>
2015-10-12 18:53:29 +1300

    Updated the code a bit.

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ var port = process.env.PORT || 3000;
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
+    if (info.pathname == '/v') {
+      res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
+    } else {
+      res.end(i + "\n");
+    }
     i = i + 0.5;
 })
 server.listen(port);
