b9a5f7401e1e535b4d9e09fb7b8d6ce535f630f9
MrFlash67 <mrflash67@noreply.github.com>
2015-10-13 09:49:15 +1300

    apis

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ var i = 0;
 var port = process.env.PORT || 3000;
 var server = http.createServer(function (req, res) {
     var info = url.parse(req.url, true);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+
     if (info.pathname == '/v') {
-      res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
+    } else if (info.pathname == "/api/json") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({"number":i}));
     } else {
-      res.end(i + "\n");
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(i + "\n");
     }
     i++;
 })
