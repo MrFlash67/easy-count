d7af6b311e48ad06eb70436c90af326dc4ac3468
MrFlash67 <mrflash67@noreply.github.com>
2015-10-12 18:51:37 +1300

    Minified the code somewhat.

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 var http = require('http');
 var i = 0;
 var port = process.env.PORT || 3000;
-console.log("hey");
-console.log(port);
+
 var server = http.createServer(function (req, res) {
-    console.log("more hey");
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
     i = i + 0.5;
 })
 server.listen(port);
-server.on('error', function (e) {
-  console.log(e);
-});
