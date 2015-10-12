bd5f94ff645cda38225b2f6e06b1f3987ddbafce
MrFlash67 <mrflash67@noreply.github.com>
2015-10-12 14:44:05 +1300

    uuh

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ var http = require('http');
 var i = 0;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(1 + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
+    res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
     i++;
 })
-server.listen(8082);
+server.listen(80);
 server.on('error', function (e) {
   // Handle your error here
   console.log(e);
 });
-
