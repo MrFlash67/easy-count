e734f0089a1dacf39cde9d55004f9a53ef89f4bf
MrFlash67 <mrflash67@noreply.github.com>
2015-10-12 19:03:11 +1300

    More cool stuff.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var url = require('url');
 var i = 0;
 var port = process.env.PORT || 3000;
-
 var server = http.createServer(function (req, res) {
+    var info = url.parse(req.url, true);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     if (info.pathname == '/v') {
       res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
