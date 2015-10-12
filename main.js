f60441952a3a42c878657d9a8243cb2893f7b40f
MrFlash67 <mrflash67@noreply.github.com>
2015-10-12 18:46:06 +1300

    uuh

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var i = 0;
+var port = process.env.PORT || 3000;
 console.log("hey");
+console.log(port);
 var server = http.createServer(function (req, res) {
     console.log("more hey");
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(i + "\n\n\n\nReload to increment.\nResets often\n" + '\n');
     i++;
 })
-//server.listen(80);
+server.listen(80);
 server.on('error', function (e) {
   console.log(e);
 });
